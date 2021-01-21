3c2abbb336e2f081c08adf31a80b96d7a35628e4
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-21 14:54:22 -0300

    Adding Navigation

---
+++ src/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const AppStack = createStackNavigator()
+
+import Login from './pages/Login'
+import Menu from './pages/Menu'
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Login" component={Login} />
+                <AppStack.Screen name="Menu" component={Menu} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
