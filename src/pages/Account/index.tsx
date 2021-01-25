a7b11dae452cdbfaf2e275d890d0f48137bb8fa5
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-25 15:44:16 -0300

    Adding Account Screen

---
+++ src/pages/Account/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+import { Background, InputField, PageTitle } from '../../styles/global'
+import { TopBar, Search, TableContainer, Table } from './styles'
+
+import ArrowLeft from '../../assets/arrow-left.svg'
+import { Text, ScrollView } from 'react-native'
+
+export default function App({ navigation }) {
+  return (
+    <Background>
+      
+      
+      <StatusBar style="dark" />
+    </Background>
+  )
+}
