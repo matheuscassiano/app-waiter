8d0de11161c2263df33b3e1b6bfc52a9b58564e9
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-27 12:53:28 -0300

    Adding SelectField

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { Picker } from '@react-native-picker/picker';
 import ToggleSwitch from 'toggle-switch-react-native'
 
 import { Background, Title, Card, PageTitle, ArrowBack } from '../../styles/global'
-import { Label, SwitchGroup } from './styles'
+import { Label, SwitchGroup, SelectField, SelectFieldContainer } from './styles'
 
 import ArrowLeft from '../../assets/arrow-left.svg'
 
@@ -46,6 +47,18 @@ export default function App({ navigation }) {
             </SwitchGroup>
             <View>
                 <Label>Selecione o toque de Pedidos</Label>
+                <SelectFieldContainer>
+                    <SelectField
+                    mode="dropdown"
+                    selectedValue={"default"}
+                    onValueChange={(itemValue, itemIndex) => console.log('oi')}>
+                        <Picker.Item label="Toque padrão" value="default" />
+                        <Picker.Item label="Toque Um" value="1" />
+                        <Picker.Item label="Toque Dois" value="2" />
+                        <Picker.Item label="Toque Três" value="3" />
+                        <Picker.Item label="Toque Quatro" value="4" />
+                    </SelectField>
+                </SelectFieldContainer>
             </View>
            <ArrowBack onPress={() => navigation.goBack()}><ArrowLeft /></ArrowBack>
             <StatusBar style="dark" />
--- src/pages/Settings/styles.ts
+++ src/pages/Settings/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { StatusBar } from 'react-native';
 import { Dimensions } from 'react-native'
+import { Picker } from '@react-native-picker/picker';
 import { PageTitle } from '../../styles/global'
 
 const windowWidth = Dimensions.get('window').width;
@@ -15,4 +16,18 @@ export const SwitchGroup = styled.View`
     margin: 8px 0;
     flex-direction: row;
     justify-content: space-between;
+`
+
+export const SelectFieldContainer = styled.View`
+    elevation: 5;
+    margin-top: 10px;
+    padding: 0 12px;
+    box-shadow: 0 0 0 black;
+    background-color: white;
+    border-radius: 12px;
+`
+
+export const SelectField = styled(Picker)`
+    color: #A0A0A0;
+    width: ${windowWidth - 88}px;
 `
