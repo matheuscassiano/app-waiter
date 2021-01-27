4d0a1bcdb92ab89598aa691e41a4a1b7b771467c
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-27 11:40:20 -0300

    Adding Switches

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import ToggleSwitch from 'toggle-switch-react-native'
 
-import { Name } from './styles'
+import { Background, Title, Card, PageTitle, ArrowBack } from '../../styles/global'
+import { Label, SwitchGroup } from './styles'
 
-import AvatarImage from '../../assets/avatar.png'
-import Star from '../../assets/star.svg'
-import { Text } from 'react-native'
-import { MidButton } from '../../components/Button'
-import { Background, Title, Card, PageTitle } from '../../styles/global'
+import ArrowLeft from '../../assets/arrow-left.svg'
 
 export default function App({ navigation }) {
+    const [ordersActive, setOrdersActive] = useState(false)
     return (
         <Background>
-           <PageTitle>Configurações</PageTitle>
+            <PageTitle>Configurações</PageTitle>
+            <Text>Notificações</Text>
+            <SwitchGroup>
+                <Label>Notificações de Pedidos</Label>
+                <ToggleSwitch
+                isOn={ordersActive}
+                onColor="#08AF24"
+                offColor="#D0D5DA"
+                size="medium"
+                onToggle={(isOn: boolean) => setOrdersActive(isOn)}
+                />
+            </SwitchGroup>
+            <SwitchGroup>
+                <Label>Notificações de Favores</Label>
+                <ToggleSwitch
+                isOn={ordersActive}
+                onColor="#08AF24"
+                offColor="#D0D5DA"
+                size="medium"
+                onToggle={(isOn: boolean) => setOrdersActive(isOn)}
+                />
+            </SwitchGroup>
+            <SwitchGroup>
+                <Label>Notificações de Cozinha</Label>
+                <ToggleSwitch
+                isOn={ordersActive}
+                onColor="#08AF24"
+                offColor="#D0D5DA"
+                size="medium"
+                onToggle={(isOn: boolean) => setOrdersActive(isOn)}
+                />
+            </SwitchGroup>
+            <View>
+                <Label>Selecione o toque de Pedidos</Label>
+            </View>
+           <ArrowBack onPress={() => navigation.goBack()}><ArrowLeft /></ArrowBack>
             <StatusBar style="dark" />
         </Background>
     )
