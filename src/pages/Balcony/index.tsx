57229f93fec2e1cc80ec0257074b9a6335ec97d9
Matheus Cassiano <matheuscassiano9@gmail.com>
2021-01-28 13:50:43 -0300

    Finaling Balcony structure

--- src/pages/Balcony/index.tsx
+++ src/pages/Balcony/index.tsx
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-import { Picker } from '@react-native-picker/picker';
-import ToggleSwitch from 'toggle-switch-react-native'
 
-import { Background, Title, Card, PageTitle, ArrowBack } from '../../styles/global'
-import { Label, SwitchGroup, SelectField, SelectFieldContainer } from './styles'
+import { Background, PageTitle, ArrowBack } from '../../styles/global'
+import { Content, Info, Token, Buttons } from './styles'
 
 import ArrowLeft from '../../assets/arrow-left.svg'
-import SettingsImage from '../../assets/settings.svg'
+import { MidButton } from '../../components/Button'
+
+import Balcony from '../../assets/balcony.svg'
 
 export default function App({ navigation }) {
-    const [ordersActive, setOrdersActive] = useState(false)
     return (
         <Background>
             <PageTitle>Pedidos no balcão</PageTitle>
-            
+            <Content>
+                <Balcony />
+                <Info>Digite um Token valide ou faça um pedido manualmente</Info>
+                <Token placeholder="Digite o Token" />
+                <Buttons>
+                    <MidButton color="#08AF24" onPress={() => console.log(false)}>Validar Token</MidButton>
+                    <MidButton color="#57C367" onPress={() => console.log(false)}>Pedir manualmente</MidButton>
+                </Buttons>
+            </Content>
            <ArrowBack onPress={() => navigation.goBack()}><ArrowLeft /></ArrowBack>
             <StatusBar style="dark" />
         </Background>
--- src/pages/Balcony/styles.ts
+++ src/pages/Balcony/styles.ts
@@ -1,32 +1,26 @@
 import styled from 'styled-components/native';
 import { Dimensions } from 'react-native'
-import { Picker } from '@react-native-picker/picker';
+import { ButtonContainer, InputField } from '../../styles/global';
+
 
 const windowWidth = Dimensions.get('window').width;
 
-export const Label = styled.Text`
-    font-size: 16px;
-    margin-top: 10px;
+export const Content = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
 `
 
-export const SwitchGroup = styled.View`
-    width: 100%;
-    padding: 10px 0;
-    flex-direction: row;
-    align-items: flex-end;
-    justify-content: space-between;
+export const Token = styled(InputField)`
+    width: ${windowWidth - 75}px;
 `
 
-export const SelectFieldContainer = styled.View`
-    margin-bottom: 10px;
-    padding: 0 12px;
-    border-radius: 16px;
-    background-color: white;
-    border: solid 1px rgba(0, 0, 0, .2);
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+export const Info = styled.Text`
+    padding: 16px 32px;
+    font-size: 18px;
+    text-align: center;
 `
 
-export const SelectField = styled(Picker)`
-    color: #A0A0A0;
-    width: ${windowWidth - 88}px;
+export const Buttons = styled(ButtonContainer)`
+    margin-top: 0;
 `
